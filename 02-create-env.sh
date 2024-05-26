1536ed566518e3314b0fd02f5a745853a71ef87a
MinKhant610 <minkhantiopp@gmail.com>
2024-05-26 12:13:55 +0630

    02-create-env-var

---
+++ 02-create-env.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+echo "current user : $USER"
+echo "login name : $LOGNAME"
+echo "home directroy : $HOME"
+echo "default langauge : $LANG"
+echo "terminal emulation : $TERM"
+echo "default shell : $SHELL"
+echo "location of command : $PATH"
+echo "my home town : $HOMETOWN"
+
+
