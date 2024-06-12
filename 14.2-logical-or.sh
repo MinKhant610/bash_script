ae0da680e3e45590cee1763e5c90c43f43bf0f7e
MinKhant610 <minkhantiopp@gmail.com>
2024-06-12 20:46:28 +0630

    14-logical-or-execution-operator

---
+++ 14.2-logical-or.sh
@@ -0,0 +1,15 @@
+#!/bin/bash 
+
+name="MinKhant"
+pass="123456"
+
+read -p "Enter your name :" user_name
+read -p "Enter your password :" password
+
+if [ $user_name == $name ] || [ $password == $pass ]
+then
+	echo "Access Granted"
+else
+	echo "Check your name or password"
+fi
+
